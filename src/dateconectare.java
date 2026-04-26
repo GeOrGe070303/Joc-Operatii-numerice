68501300b592085436c77ea59d55573e7f9857d1
George Marc <georgemarc07@yahoo.com>
2026-04-26 22:05:58 +0300

    modificare date

--- src/dateconectare.java
+++ src/dateconectare.java
@@ -1,5 +1,5 @@
 public class dateconectare {
-    static String PAROLA = "PaRoLa@03";
+    static String PAROLA = ""; // AICI PUI PAROLA TA MYSQL
     static String LOCALIZARE = "jdbc:mysql://localhost:3306/Joc";
-    static String NUMEUTILIZATOR  = "root";
+    static String NUMEUTILIZATOR  = ""; // AICI PUI NUMELE TAU DE UTILIZATOR MYSQL
 }
